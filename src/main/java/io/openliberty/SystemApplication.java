da6131d0af2bd012d793c4e970bc473e81e8ac37
Prince Carter <prince@ibm.com@princes-mbp.austin.ibm.com>
2019-05-31 10:46:38 -0500

    Initial Commit

---
+++ src/main/java/io/openliberty/SystemApplication.java
@@ -0,0 +1,9 @@
+package io.openliberty;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("System")
+public class SystemApplication extends Application {
+
+}
